3125da4dfc9adc8b51365d25f6e660eceb81602b
Andrei Zarmin De Jesus <161214656+DreiZzzzz@users.noreply.github.com>
2025-02-17 14:04:57 +0800

    grouped files

--- PyShop/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views # import views module from the same folder
-
-urlpatterns = [
-    path('', views.index), # '' => root url
-    path('new', views.new)
-]
